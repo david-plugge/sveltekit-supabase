45cdd57ad2dafaef22a903fd851873956c5e7f07
David Plugge <git@davidplugge.de>
2022-09-09 17:04:24 +0200

    disable autoRefreshToken

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,5 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-	persistSession: false
+	persistSession: false,
+	autoRefreshToken: false
 });
