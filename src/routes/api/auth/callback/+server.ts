0a5e541af20d425937a00a8520fb75371af0e0b1
David Plugge <git@davidplugge.de>
2022-09-10 18:05:06 +0200

    note that there is a bug

--- src/routes/api/auth/callback/+server.ts
+++ src/routes/api/auth/callback/+server.ts
@@ -1,4 +1,5 @@
 import type { RequestHandler } from './$types';
 import { handleCallbackSession } from '$lib/supabase/server';
 
+// TODO: remove this when the cookie bug if fixed
 export const POST: RequestHandler = handleCallbackSession;
